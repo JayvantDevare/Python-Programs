b9ae20792973d05f1d92b1bdfbefa2a180cde30b
Jayvant Devare <jayvant.devare@gmail.com>
2020-03-29 18:51:17 +0530

    Add files via upload
    Python Program to print "Hello World!!"

---
+++ P1-Print_Hello_World.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:     hello world! program
+#
+# Author:      jayvant devare
+#
+# Created:     29/03/2020
+# Copyright:   (c) jayvant 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print("Hello Woprld!!!!!")
